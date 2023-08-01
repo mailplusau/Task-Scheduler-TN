92aabd30c9e236a37dac60b8f67be387fd3a6953
Thien Nguyen <s3426515@rmit.edu.vn>
2023-08-02 01:02:34 +1000

    Add scheduler utility

---
+++ src/utils/scheduler.mjs
@@ -0,0 +1,14 @@
+export const scheduler = {
+    toNSSearchableDatetime : (NS_MODULES, date) => {
+        let formatted = NS_MODULES.format.format({ value: date, type: NS_MODULES.format.Type.DATETIMETZ });
+
+        return formatted.replace(/(:\d{2}):\d{1,2}\b/, "$1");
+    },
+    dispatchTask : (NS_MODULES, {taskRecordId, taskType, scriptId, deploymentId}) => {
+        let params = {};
+        params[`custscript_${scriptId}_task_record_id`] = taskRecordId;
+
+        let scriptTask = NS_MODULES.task.create({taskType, scriptId, deploymentId, params});
+        scriptTask.submit();
+    }
+};
